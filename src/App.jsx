c2bfb789baea3c29c2ba1ec1ea4a70e1a72bedf7
Reyes_Works <RP191495@alumno.udb.edu.sv>
2023-10-27 11:30:45 -0600

    Se tiene la funcionalidad de mostras las criptomonedas en cuadricula.

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,11 @@
  * 
  */
 // El UseEffect se usa para cuando el componente se contruira.
+
 import { useEffect, useState } from "react"
 import axios from "axios"
 import "./App.css"
+import Cripto from "./Cripto";
 
 function App() {
 
@@ -42,17 +44,22 @@ function App() {
   if(!criptos) return <span>Cargando...</span>
 
   return (
-    <>
-    <h1>Lista de criptomonedas</h1>
-     {/*En JSX se puede usar JavaScrip para hacer la parte de diseño de tablas.*/}
-      <ol>
-        { 
-        criptos.map(({id, name, priceUsd}) => (
-           <li key={id /*Para poder identificar cada li, se debe usar el props key al cual se le asigna un id unica.*/}>Nombre: {name} Precio: {priceUsd}</li>
-        ))
-        }
-      </ol>
-   </>
+    <div className="app-container">
+      <h1>Lista de criptomonedas</h1>
+           {/*En JSX se puede usar JavaScrip para hacer la parte de diseño de tablas.*/}
+          <div className="cripto-container">
+          { 
+            criptos.map(({id, name, priceUsd, symbol, changePercent24Hr}) => (
+            <Cripto 
+              key={id} 
+              name={name}
+              priceUSD={priceUsd} 
+              symbol={symbol} 
+              changePercent24Hr={changePercent24Hr}/>
+            ))
+          }
+      </div>
+   </div>
    
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-
+//La buena practica es que en este archivo se importen los archivos bases de nuestra aplicacion.
+import "./main.css";
 ReactDOM.createRoot(document.getElementById('root')).render(< App/>)
